b3d14d01f96f29d73eb9bc2db3bb3a993c404a28
aayushali <aayushkhan51@gmail.com>
2021-09-05 17:13:48 +0545

    animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <section>
         <container className="grid grid-cols-3 gap-4 h-screen my-20">
           <div
-            className="left-image"
+            className="left-image "
             style={{
               backgroundImage: `url(/images/home-page-left.png)`,
               backgroundRepeat: "no-repeat",
@@ -20,22 +20,30 @@ const Hero = () => {
                 <img
                   src="/images/home-left-top1.png"
                   alt=""
-                  className="absolute bottom-0.5 left-8  h-24"
+                  className="absolute bottom-0.5 left-8  h-24 animate__animated animate__slideInDown animate__infinite animate__slow animate__delay-3s"
                 />
               </div>
               <div>
                 <img
                   src="/images/home-left-top2.png"
                   alt=""
-                  className="absolute bottom-0 h-24"
+                  className="absolute bottom-0 h-24  animate__animated animate__slideInDown animate__infinite animate__slow animate__delay-3s"
                 />
               </div>
             </div>
           </div>
-          <div className="center-text text-green-600 ">
+          <div className="center-text text-green-600 mx-20 my-10 ">
             <div>
-              <img src="/images/home-left-top.png" alt="" className="h-32" />
-              <img src="/images/home-left-top3.png" alt="" className="w-40" />
+              <img
+                src="/images/home-left-top.png"
+                alt=""
+                className="h-32  animate__animated animate__slideInDown animate__infinite animate__slow animate__delay-3s"
+              />
+              <img
+                src="/images/home-left-top3.png"
+                alt=""
+                className="w-40  animate__animated animate__slideInDown animate__infinite animate__slow animate__delay-3s"
+              />
             </div>
             <h1 className="text-5xl leading-relaxed">Sharing the &nbsp;</h1>
             <span className="text-6xl text-black font-bold block py-4">
@@ -47,7 +55,7 @@ const Hero = () => {
               <img
                 src="/images/home-page-center-bottom.png"
                 alt=""
-                className="h-32 w-40 mx-auto my-8"
+                className="h-32 w-40 mx-auto my-8  animate__animated animate__slideInDown animate__infinite animate__slow animate__delay-3s"
               />
             </div>
           </div>
@@ -60,10 +68,18 @@ const Hero = () => {
             }}
           >
             <div>
-              <img src="/images/Home-page-right-top1.png" alt="" />
+              <img
+                src="/images/Home-page-right-top1.png"
+                alt=""
+                className="absolute h-28 w-40 right-3  animate__animated animate__slideInDown animate__infinite animate__slow animate__delay-3s"
+              />
             </div>
             <div>
-              <img src="" alt="" />
+              <img
+                src="/images/Home-page-right-bottom.png"
+                alt=""
+                className="absolute bottom-0  animate__animated animate__slideInDown animate__infinite animate__slow animate__delay-3s"
+              />
             </div>
           </div>
         </container>
